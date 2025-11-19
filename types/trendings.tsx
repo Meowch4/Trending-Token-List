73f50606b34e1115ffdfc37040ad67bcb331762f
Matcha <Matcha@SuperPlatypus.com>
2025-11-19 20:58:24 +0800

    add TrendingToken type

---
+++ types/trendings.tsx
@@ -0,0 +1,19 @@
+export type TrendingToken = {
+  baseDecimals: number;
+  baseName: string;
+  baseSymbol: string;
+  liquidity: number;
+  marketCap: number;
+  price: number;
+  priceUsd: number;
+  priceChange1h: number;
+  priceChange24h: number;
+  volumeUsd24h: number;
+  pair: string;
+  chainId: string;
+  baseToken: string;
+  quoteToken: string;
+  dex: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [k: string]: any;
+}
